1777fdf22fecf72047b0fceb79629d1bda271a8f
Victor Carneiro <victorhugo.carneiro86@gmail.com>
2023-06-22 17:17:40 -0300

    findByld implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,21 @@
 package application;
 
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		//Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21,"bob", "bob@gmail", null, 3000.0, obj);
+		//Seller seller = new Seller(21,"bob", "bob@gmail", null, 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
