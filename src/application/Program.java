a9454da378f52222328481c7afe06f222aa720c4
Victor Carneiro <victorhugo.carneiro86@gmail.com>
2023-06-22 20:12:40 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -14,6 +15,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		//Department obj = new Department(1, "Books");
 		
 		//Seller seller = new Seller(21,"bob", "bob@gmail", null, 3000.0, obj);
@@ -47,6 +49,14 @@ public class Program {
 		seller.setName("Jhonny Walker");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 		
 		}
 	}
